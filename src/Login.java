b9c4781dcb98063473e7285655a823bdbd3fcbcd
Yuri Vissoto <vissotoyuri@gmail.com>
2024-09-07 18:03:03 -0300

    Single-Sign-On

---
+++ src/Login.java
@@ -0,0 +1,7 @@
+import Yuri.LoginYuri;
+
+public class Login {
+    public static void main ( String args [] ) {
+        new LoginYuri().setVisible(true);
+    }
+}
---
+++ src/constants/CommonConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class CommonConstants {
+    public final static String APP_NAME = "Login";
+
+    public final static int[] FRAME_SIZE = {400, 400};
+
+    public final static int TEXTFILD_SIZE = 15;
+
+}
